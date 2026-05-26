31d9c623ec005e8c575cf325b3d10710ced1ac62
Henrique <henrique@sidekick-coder.com>
2026-05-26 17:32:14 -0300

    feat: end video

---
+++ src/AddressField.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { useField } from 'vee-validate';
+
+const props = defineProps({
+    name: {
+        type: String,
+        required: true
+    }
+})
+
+const { value: state } = useField(`${props.name}.state`);
+const { value: city } = useField(`${props.name}.city`);
+const { value: address } = useField(`${props.name}.address`);
+const { value: number } = useField(`${props.name}.number`);
+</script>
+<template>
+  <div class="flex flex-col gap-y-2">
+    <label class="flex justify-between">
+        <span>state</span>
+        <input class="w-[80%]" v-model="state" />
+    </label>
+    <label class="flex justify-between">
+        <span>city</span>
+        <input class="w-[80%]" v-model="city" />
+    </label>
+    <label class="flex justify-between">
+        <span>address</span>
+        <input class="w-[80%]" v-model="address" />
+    </label>
+    <label class="flex justify-between">
+        <span>number</span>
+        <input type="number" class="w-[80%]" v-model="number" />
+    </label>
+  </div>
+</template>
--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,59 @@
-<script setup></script>
+<script setup>
+import { useForm, Field } from 'vee-validate';
+import { toTypedSchema } from '@vee-validate/valibot'
+import * as v from 'valibot'
+import AddressField from './AddressField.vue';
+import { ref } from 'vue';
 
+const schema = v.object({
+  name: v.pipe(v.string(), v.minLength(3)),
+  email: v.pipe(v.string(), v.email()),
+  age: v.number(),
+  gender: v.picklist(['male', 'female']),
+  address: v.object({
+    city: v.string(),
+    state: v.string(),
+    address: v.string(),
+    number: v.number(),
+  })
+})
+
+const result = ref()
+
+const { handleSubmit, errors } = useForm({
+  validationSchema: toTypedSchema(schema)
+})
+
+const onSubmit = handleSubmit((data) => {
+  result.value = data
+})
+
+</script>
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-</template>
+  <div class="flex h-screen items-center justify-center">
+    <form @submit="onSubmit" class="flex flex-col gap-y-4 border p-4 rounded border-zinc-500 w-full max-w-md">
+      <h1 class="font-bold">Sign up</h1>
+      <Field type="text" name="name" />
+      <Field type="text" name="email" />
+      <Field type="number" name="age" />
+      <Field name="gender" as="select">
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+      </Field>
 
-<style scoped></style>
+      <AddressField name="address" />
+
+      <button type="submit">Submit</button>
+
+      <div>
+        <div v-for="(e, key) of errors" class="text-red-500">
+          {{key}}: {{  e }}
+        </div>
+      </div>
+    </form>
+
+    <div v-if="result" class="fixed inset-0 bg-black/80 flex items-center justify-center" @click="result = null">
+      <pre class="bg-white rounded p-4">{{ result }}</pre>
+    </div>
+  </div>
+</template>
